7d70b127326d75edaed33258bbff1f39ab0a98d8
shivam <shivammaniya7469@gmail.com>
2022-06-20 15:29:12 -0400

    second commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,7 @@
 module.exports = 
 {
-    "URI": "mongodb://localhost/27017",
-    // "URI": "mongodb+srv://thomas:Hq3DKYN2NaAqGyyV@mongodbserver.k15hk.azure.mongodb.net/book_store?retryWrites=true&w=majority",
+    // "URI": "mongodb://localhost/27017",
+    
+    "URI": "mongodb+srv://shivam__09:lbSkIIggbasWU9r9@cluster0.cyhwz.mongodb.net/portfoilo?retryWrites=true&w=majority",
     "Secret": 'SomeSecret'
 }
